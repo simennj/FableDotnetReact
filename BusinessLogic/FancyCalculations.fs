8ae32a8780bb64498236ed264a588e268f5ebdc0
Simen Norderud Jensen <simen.jensen@bouvet.no>
2021-11-09 22:09:36 +0100

    add F# shared code project

---
+++ BusinessLogic/FancyCalculations.fs
@@ -0,0 +1,16 @@
+﻿module BusinessLogic.FancyCalculations
+
+type AnInputType = {
+    AnInputField: decimal;
+    AnotherInputField: decimal;
+}
+
+type AnOutputType = {
+    AnOutputField: decimal;
+    AnotherOutputField: decimal;
+}
+
+let calculateSomeFancyStuff (input: AnInputType) =
+    let anOutputField = input.AnInputField + input.AnotherInputField;
+    let anotherOutputField = input.AnInputField * input.AnotherInputField;
+    { AnOutputField = anOutputField; AnotherOutputField = anotherOutputField }
