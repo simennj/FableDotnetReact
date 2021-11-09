84bfb70bf750cee26e9d169f65a176530bd6928c
Simen Norderud Jensen <simen.jensen@bouvet.no>
2021-11-09 21:41:21 +0100

    prepare dotnet project

---
+++ WebApplication/Controllers/FancyCalculationsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FancyCalculationsController : ControllerBase
+    {
+        [HttpGet]
+        public object Get() => throw new NotImplementedException();
+    }
+}
