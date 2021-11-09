2b7f08490cdcf2c013ba3afa6f81a1fee33c5508
Simen Norderud Jensen <simen.jensen@bouvet.no>
2021-11-09 21:31:50 +0100

    prepare react project

---
+++ WebApplication/ClientApp/src/App.tsx
@@ -0,0 +1,33 @@
+﻿import {ChangeEvent, useCallback, useMemo, useState} from "react";
+import {calculateSomeFancyStuff} from "./BusinessLogic";
+
+export const App = () => {
+    const [input, setInput] = useState({anInputField: '0', anotherInputField: '0'})
+    const inputSetter = useCallback(({target}: ChangeEvent<HTMLInputElement>) => {
+        setInput(prevState => ({...prevState, [target.name]: target.value}))
+    }, [])
+    const output = useMemo(() => calculateSomeFancyStuff({
+        anInputField: parseFloat(input.anInputField),
+        anotherInputField: parseFloat(input.anotherInputField)
+    }), [input])
+    console.log(input, output)
+    return <>
+        <label>
+            AnInput
+            <input name='anInputField' value={input.anInputField} onChange={inputSetter}/>
+        </label>
+        <label>
+            AnotherInput
+            <input name='anotherInputField' value={input.anotherInputField} onChange={inputSetter}/>
+        </label>
+        <hr/>
+        <label>
+            AnOutput
+            <input value={output.anOutputField} readOnly={true}/>
+        </label>
+        <label>
+            AnotherOutput
+            <input value={output.anotherOutputField} readOnly={true}/>
+        </label>
+    </>
+}
---
+++ WebApplication/ClientApp/src/BusinessLogic.ts
@@ -0,0 +1,15 @@
+﻿export interface AnInputType {
+    anInputField: number;
+    anotherInputField: number;
+}
+
+export interface AnOutputType {
+    anOutputField: number;
+    anotherOutputField: number;
+}
+
+export const calculateSomeFancyStuff: (input: AnInputType) => AnOutputType = input => {
+    const anOutputField = input.anInputField + input.anotherInputField;
+    const anotherOutputField = input.anInputField * input.anotherInputField;
+    return { anOutputField, anotherOutputField };
+};
